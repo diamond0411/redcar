cdadf4da18b92ce438c9b6e3820ba943a9c84e23
diamond0411 <starwars0411@gmail.com>
2025-02-04 13:24:03 -0500

    controller looking done

--- backend/src/llmQuery/controllers/llmquery.controller.ts
+++ backend/src/llmQuery/controllers/llmquery.controller.ts
@@ -1,7 +1,15 @@
-import {Controller, Post, Body} from '@nestjs/common';
+import {Controller, Post, Body, Sse} from '@nestjs/common';
 import {LLMQueryDTO, LLMResponseDTO} from '../dto/llmquery.dto'
+import { LLMQueryService } from '../services/llmquery.service';
+import { Observable } from 'rxjs';
 
 @Controller("llmquery")
 export class LLMQueryController {
-    constructor(private readonly queryService: )
+    constructor(private readonly llmQueryService: LLMQueryService) {}
+
+    @Post('stream')
+    @Sse('stream')
+    async stream(@Body() body: {prompt: string, domain: string}): Promise<Observable<MessageEvent>> {
+        return this.llmQueryService.streamLLMResponse(body.prompt, body.domain);
+    }
 }
