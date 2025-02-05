04072714cf9299f4641a65d77848917269c20181
diamond0411 <starwars0411@gmail.com>
2025-02-05 01:28:47 -0500

    Adding Users

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema, User } from './entities/user.entity';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: User.name, schema:
+    UserSchema }])],
   controllers: [UserController],
   providers: [UserService],
 })
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,26 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './entities/user.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  async create(createUserDto: CreateUserDto) {
+    const user = await this.userModel.create(createUserDto);
+    return user.save();
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async findAll() {
+    const users = this.userModel.find();
+    return users;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async findOne(id: number) {
+    const user = await this.userModel.findOne({ where: { id: id } });
+    if (user) {
+      return user;
+    }
+    throw new NotFoundException('user not found');
   }
 }
