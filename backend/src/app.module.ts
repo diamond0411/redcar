561ce1f7e4d36483ae83104484fea7f61593725a
diamond0411 <starwars0411@gmail.com>
2025-02-05 01:21:45 -0500

    user and auth

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { LLMQueryModule } from "./llmQuery/llmQuery.module"
 import { ConfigModule } from '@nestjs/config';
 import { LoggerMiddleware } from './middleware/logger.middleware';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true, envFilePath: '.env'}),LLMQueryModule],
+  imports: [ConfigModule.forRoot({isGlobal: true, envFilePath: '.env'}),LLMQueryModule, UserModule, AuthModule],
   controllers: [],
   providers: [],
 })
