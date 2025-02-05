f720527edca8b0b42cd7543224d57a6ad8fb9174
diamond0411 <starwars0411@gmail.com>
2025-02-05 01:25:06 -0500

    mongodb

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,16 @@ import { ConfigModule } from '@nestjs/config';
 import { LoggerMiddleware } from './middleware/logger.middleware';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true, envFilePath: '.env'}),LLMQueryModule, UserModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true, envFilePath: '.env'}),
+    LLMQueryModule, 
+    UserModule, 
+    AuthModule,
+    MongooseModule.forRoot(process.env.MONGO_URL+'/redcar')
+  ],
   controllers: [],
   providers: [],
 })
